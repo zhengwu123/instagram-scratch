6dabb0a83b62bd91f5098ef38ca68d340ad1ae05
wu <wu673@purdue.edu>
2016-02-28 13:06:42 -0500

    fisrt commit

---
+++ CourseRater/photoCell.swift
@@ -0,0 +1,24 @@
+//
+//  photoCell.swift
+//  CourseRater
+//
+//  Created by New on 2/27/16.
+//  Copyright © 2016 CodeMonkey. All rights reserved.
+//
+
+import UIKit
+
+class photoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
